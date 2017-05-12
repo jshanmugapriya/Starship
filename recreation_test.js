cea0bff41ed084a38f6bcc66835c89c65c0bb0a0
jshanmugapriya <jayakumarshanmugapriya@gmail.com>
2017-05-11 20:56:00 -0700

    Create recreation_test.js

---
+++ recreation_test.js
@@ -0,0 +1,34 @@
+var AWS = require('aws-sdk');
+var dynamodb = new AWS.DynamoDB();
+
+exports.handler = (event, context) => {
+
+    //table = dynamodb.Table('recreationpark_users')
+    console.log("Hello world");
+    /*response = dynamodb.getItem({
+        TableName: 'recreationpark_users',
+        Key: {
+            email : {
+                S: event.email
+            }
+        }
+    }, function(err, data) {
+            if (err) {
+                console.log('the user returned err');
+                context.fail('returned err' + err);
+            } else {
+                if ('Item' in data) {
+                    console.log(data.Item);
+                    context.succeed({
+                        exist: true,
+                        item: data.Item
+                    });
+                } else {
+                    console.log('the user is no longer active/available');
+                    context.succeed({
+                        exist: false
+                    });
+                }
+            }
+        });*/
+};
